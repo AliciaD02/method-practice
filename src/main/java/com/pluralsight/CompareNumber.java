640529ec519aa6332f6e9b9b57f4236a4c96e259
AliciaD02 <diazalicia4542@gmail.com>
2026-04-13 20:52:00 -0500

    first comment

--- src/main/java/com/pluralsight/CompareNumber.java
+++ src/main/java/com/pluralsight/CompareNumber.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class CompareNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(isEven(7));
+
+    }
+        public static boolean isEven(int someNumber){
+            if(someNumber %2 == 0){
+                // positive number
+                return true;
+
+            } //negative number
+            return false;
+
+        }
+
 }
--- src/main/java/com/pluralsight/Format.java
+++ src/main/java/com/pluralsight/Format.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Format {
+    public static void main(String[] args) {
+
+      String formatName = formatName("Alicia","Diaz");
+        System.out.println(formatName);
+    }
+
+    public static String formatName(String firstName,String lastName){
+      return lastName + "," + firstName;
+
+    }
 }
